e09787be63a679d38cda1361fc1f77dd39f157da
Vladimir Zabara <wlady2001@gmail.com>
2016-05-31 19:03:38 +0300

    Init Project

---
+++ public_html/protected/views/site/error404.php
@@ -0,0 +1,11 @@
+<?php
+/* @var $this SiteController */
+/* @var $error array */
+
+$this->pageTitle=Yii::app()->name . ' - Error';
+$this->breadcrumbs=array(
+	'Error',
+);
+?>
+
+<h2>Error <?php echo $code; ?></h2>
